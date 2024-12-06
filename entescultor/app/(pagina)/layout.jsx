7ccfbf6a2817e9f686df91f99028fb461a42a7a7
carlos <carlosperezcruz01@gmail.com>
2024-12-06 21:00:45 +0100

    cambiados js a jsx y tamaños y tipo de fuente

--- entescultor/app/(pagina)/layout.jsx
+++ entescultor/app/(pagina)/layout.jsx
@@ -11,6 +11,7 @@ export default function RootLayout ({ children }) {
         <title>ENTESTULTOR</title>
         <meta name='description' content='Página de esculturas de Indalecio Pérez Entrena' />
         <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.8.1/css/all.css' />
+        <link href="https://fonts.googleapis.com/css2?family=Crimson+Text:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&family=Geist:wght@100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
         <meta charSet='utf-8' />
         <link rel='shortcut icon' href={`${process.env.NEXT_PUBLIC_API_URL}/images/icon/ent.jpg`} />
       </head>
