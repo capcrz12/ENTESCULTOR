4ce3bae21deffd36dbe1da5468f38cba64c121c4
carlos <carlosperezcruz01@gmail.com>
2024-12-07 18:28:32 +0100

    nodemailer

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -6,9 +6,7 @@ const Usuario = require('../models/Usuario')
 
 //Configuración de nodemailer
 const transporter = nodemailer.createTransport({
-  host: 'smtp.ethereal.email',
-  port: 587,
-  secure: false, // true for port 465, false for other ports  
+  service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
