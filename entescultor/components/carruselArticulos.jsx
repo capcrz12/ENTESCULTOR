cacd0400958d984d3b768fb776f446c98cf73ab2
Carlos Pérez Cruz <carlosperezcruz01@gmail.com>
2025-05-24 13:25:33 +0200

    autor y articulos css mejorado

--- entescultor/components/carruselArticulos.jsx
+++ entescultor/components/carruselArticulos.jsx
@@ -1,21 +1,21 @@
-'use client'
+"use client";
 
-import Link from 'next/link'
-import styles from '@/styles/articulos.module.css'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
-import React from 'react'
-import Slider from 'react-slick'
-import { RViewer, RViewerTrigger } from 'react-viewerjs'
+import Link from "next/link";
+import styles from "@/styles/articulos.module.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import React, { useEffect } from "react";
+import Slider from "react-slick";
+import { RViewer, RViewerTrigger } from "react-viewerjs";
 
-export default function CarruselArticulos ({ articulos }) {
+export default function CarruselArticulos({ articulos }) {
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
-    slidesToScroll: 1
-  }
+    slidesToScroll: 1,
+  };
 
   const options = {
     toolbar: {
@@ -26,32 +26,41 @@ export default function CarruselArticulos ({ articulos }) {
       rotateRight: false,
       flipHorizontal: false,
       flipVertical: false,
-      oneToOne: false
-    }
-  }
+      oneToOne: false,
+    },
+  };
+
+  useEffect(() => {
+    console.log(articulos);
+  }, [articulos]);
 
   return (
     <Slider {...settings} className={styles.seccion}>
-      {articulos.map(articulo => (
+      {articulos.map((articulo) => (
         <article key={articulo.id} className={styles.completo}>
           <h3 className={styles.title}>{articulo.title}</h3>
           <div className={styles.fecha}>{articulo.fecha}</div>
           <div className={styles.texto}>{articulo.texto}</div>
           <div className={styles.div_image}>
-            {
-              articulo.image !== ''
-                ?
-                <RViewer options={options} imageUrls={`${articulo.image}`}>
-                  <RViewerTrigger>
-                    <img alt='No disponible' src={`${articulo.image}`} className={styles.image} />
-                  </RViewerTrigger>
-                </RViewer>
-                : ''
-            }
+            {articulo.image !== "" && articulo.image !== undefined ? (
+              <RViewer options={options} imageUrls={`${articulo.image}`}>
+                <RViewerTrigger>
+                  <img
+                    alt="No disponible"
+                    src={`${articulo.image}`}
+                    className={styles.image}
+                  />
+                </RViewerTrigger>
+              </RViewer>
+            ) : (
+              ""
+            )}
           </div>
-          <Link className={styles.enlace} href={articulo.url} target='_blank'>- Enlace al artículo -</Link>
+          <Link className={styles.enlace} href={articulo.url} target="_blank">
+            - Enlace al artículo -
+          </Link>
         </article>
       ))}
     </Slider>
-  )
+  );
 }
